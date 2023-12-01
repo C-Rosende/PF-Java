6235ab55c57a12ddcb5a7a3149ac664c92a09091
Clemente Rosende <cleme.rosende@gmail.com>
2023-12-01 20:16:21 -0300

    Cambio de var a let . creacion de array.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var app = {
+let app = {
   form: document.getElementById("form"),
   amountInput: document.getElementById("amount"),
   fromSelect: document.getElementById("from"),
@@ -6,14 +6,15 @@ var app = {
   resultDiv: document.getElementById("result"),
   errorDiv: document.getElementById("error"),
   apiURL: "https://v6.exchangerate-api.com/v6/b7ce959c6a2fee836009ded6/latest/",
+  currencies: [],
 
   getRates: function(from, callback) {
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("GET", this.apiURL + from);
     xhr.send();
     xhr.onload = function () {
       if (xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
+        let response = JSON.parse(xhr.responseText);
         callback(response.conversion_rates);
       } else {
         this.showError("Error: " + xhr.statusText);
@@ -23,16 +24,16 @@ var app = {
 
   formatNumber: function(num) {
     num = Number(num);
-    var parts = num.toString().split(".");
+    let parts = num.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     return parts.join(",");
   },
 
   showResult: function(amount, from, to, rate) {
-    var converted = amount * rate[to];
-    var formattedConverted = this.formatNumber(converted);
-    var formattedAmount = this.formatNumber(amount);
-    var message = `${formattedAmount} ${from} = ${formattedConverted} ${to}`;
+    let converted = amount * rate[to];
+    let formattedConverted = this.formatNumber(converted);
+    let formattedAmount = this.formatNumber(amount);
+    let message = `${formattedAmount} ${from} = ${formattedConverted} ${to}`;
     this.resultDiv.textContent = message;
     this.resultDiv.style.display = "block";
     this.errorDiv.style.display = "none";
@@ -46,9 +47,9 @@ var app = {
 
   handleSubmit: function(event) {
     event.preventDefault();
-    var amount = Number(this.amountInput.value);
-    var from = this.fromSelect.value;
-    var to = this.toSelect.value;
+    let amount = Number(this.amountInput.value);
+    let from = this.fromSelect.value;
+    let to = this.toSelect.value;
     if (amount > 0) {
       this.getRates(from, function (rates) {
         this.showResult(amount, from, to, rates);
@@ -61,9 +62,9 @@ var app = {
   init: function() {
     this.form.addEventListener("submit", this.handleSubmit.bind(this));
     this.getRates("USD", function (rates) {
-      var currencies = Object.keys(rates);
-      for (var currency of currencies) {
-        var option = document.createElement('option');
+      this.currencies = Object.keys(rates);
+      for (let currency of this.currencies) {
+        let option = document.createElement('option');
         option.value = currency;
         option.textContent = currency;
         this.fromSelect.appendChild(option);
@@ -71,13 +72,13 @@ var app = {
       }
     }.bind(this));
 
-    var invertButton = document.getElementById("invert");
+    let invertButton = document.getElementById("invert");
     invertButton.addEventListener("click", this.invertCurrencies.bind(this));
   },
 
   invertCurrencies: function() {
-    var fromCurrency = this.fromSelect.value;
-    var toCurrency = this.toSelect.value;
+    let fromCurrency = this.fromSelect.value;
+    let toCurrency = this.toSelect.value;
     this.fromSelect.value = toCurrency;
     this.toSelect.value = fromCurrency;
     this.handleSubmit(new Event('submit'));
